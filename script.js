69c6c7e281bcb09d93dfdaa01597dbdec03d9f6e
Submarino1 <a22050027@unisierra.edu.mx>
2025-05-07 18:34:59 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,10 @@ function toggleChat() {
     const chatBox = document.getElementById('chatBox');
     chatBox.classList.toggle('chat-visible');
   }
+  // Function to close the sticky ad
+  function cerrarAnuncio() {
+    const overlay = document.getElementById('anuncioOverlay');
+    overlay.style.display = 'none';
+   }
+
+   
